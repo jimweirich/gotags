e33ade4e2e5a899da6d4daac7a53aa0f3744dd90
Jim Weirich <jim.weirich@gmail.com>
2013-04-27 18:36:36 -0400

    Bump to beta version 1.1.1.beta.1

--- src/onestepback.org/gotags/gotags.go
+++ src/onestepback.org/gotags/gotags.go
@@ -40,7 +40,7 @@ func walkDir(writer *bufio.Writer, path string, info os.FileInfo, err error) err
 	return nil
 }
 
-var version = "1.1.0"
+var version = "1.1.1.beta.1"
 
 func main() {
 	var showVersion bool = false
